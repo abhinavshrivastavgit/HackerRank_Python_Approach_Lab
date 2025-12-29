b6a7e0e24f9c1592242002a09640447fcd2b164c
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2025-12-30 01:33:07 +0530

    Refactor: remove duplicate files and organize folder structure

--- integer_division.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    
-    # First line: Integer Division
-    print(a // b)
-    
-    # Second line: Float Division
-    print(a / b)
