5a0617f404985ebb1afa3960fbf8c2f4cfc58336
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2025-12-30 00:59:27 +0530

    chore: save all introduction and basic data type files

---
+++ HackerRank_Python_Approach_Lab/01_Introduction/integer_division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # First line: Integer Division
+    print(a // b)
+    
+    # Second line: Float Division
+    print(a / b)
